9fe4ff9fb6cceb367393e412ddffcfb8aca1ec3f
tako0907 <nagataki@s14.yeskist.net>
2015-05-25 11:26:40 +0900

    新世代

---
+++ Janken/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibDLL;
+
+
+namespace Janken
+{
+    class Program
+    {
+        const int GAME_FPS = 30;        // ゲームのFPS
+
+        static void Main(string[] args)
+        {
+            byte[] key = new byte[256];         // 押されたキーボードの格納用配列
+
+            DX.ChangeWindowMode(DX.TRUE);       // 画面をウィンドウモードにする
+            DX.SetGraphMode(800, 600, 32);      // 画面の解像度設定
+
+            /* DXライブラリの初期化 */
+            if (DX.DxLib_Init() == -1)
+            {
+                return;     // エラーの場合は終了
+            }
+
+            DX.SetDrawScreen(DX.DX_SCREEN_BACK);        // 描画先を裏画面にする
+
+            GameData gameData = new GameData();     // ゲームクラスの初期化
+            FPS fps = new FPS(GAME_FPS);            // FPS制御クラスの初期化
+
+            gameData.GameFPS = GAME_FPS;            // FPSのセット
+
+            /* メインループ */
+            while (DX.ScreenFlip() == 0 && DX.ProcessMessage() == 0 && DX.ClearDrawScreen() == 0 && DX.GetHitKeyStateAll(out key[0]) == 0 && fps.Update())
+            {
+                if (gameData.gmprogstat == GameData.GameProgressStatus.END)     // 終了画面へ
+                    break;
+
+                switch (gameData.gmprogstat)
+                {
+                    case GameData.GameProgressStatus.StartScreen:
+                        gameData.Start_Screen(key);
+                        break;
+
+                    case GameData.GameProgressStatus.GamePlay:
+                        gameData.GamePlay(key);
+                        break;
+                }
+
+                DX.DrawString(0, 0, fps.getFPS().ToString("0.0fps"), DX.GetColor(100, 100, 100));       // FPS表示
+                fps.wait();     // 待機
+            }
+
+            DX.DxLib_End();     // DXライブラリの終了
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# janken
+# Project Name
+Janken
+
+# Overview
+じゃんけんゲームができるプログラム
+
+# Team Member
+## プログラム担当
+- Tako0907
+- JIKLorz
+- kitaKist
+
+## デザイン担当
+- yuripe
+- aoichan
+
+# 使用言語
+C#
+
+# 使用ライブラリ
+C#用 DXライブラリ
+
+# クローンについて
+ 1. [C#用 DXライブラリをダウンロード](http://homepage2.nifty.com/natupaji/DxLib/dxdload.html "本家サイト")し、解凍しておく
+ 2. Visual Studioへプロジェクトを持ってくる
+ 3. C#用 DXライブラリの中にある以下の二つのファイルをコピー
+   - DxLib.dll
+   - DxLibDotNet.dll
+ 4. コピーしたファイルを以下の場所へ入れる
+   - \プロジェクトフォルダ\bin\Debug\
+ 5. プロジェクトでDxLib.dllが参照追加されたことを確認する（エラーが消えたことを確認）
+
+# デバック時
+ 画像（imgフォルダ）をDebugフォルダの直下に配置してください。
+
+# 方針
+簡単なプログラムを様々なアイデアをくっつけるスタイルで行く
+ 
